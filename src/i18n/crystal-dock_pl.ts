1238292645a8399a773c4a0d32141c051980613e
aquilapl <81761491+aquilapl@users.noreply.github.com>
2026-04-10 14:36:25 +0200

    Add files via upload
    Spolszczenie

---
+++ src/i18n/crystal-dock_pl.ts
@@ -0,0 +1,448 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>AddPanelDialog</name>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="14"/>
+        <source>Add Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="45"/>
+        <source>Panel position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="62"/>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="67"/>
+        <source>Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="72"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="77"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="91"/>
+        <source>Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="114"/>
+        <source>Optional features:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="127"/>
+        <source>Application Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="143"/>
+        <source>Pager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="159"/>
+        <source>Please set up your new panel.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="172"/>
+        <source>Clock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="188"/>
+        <source>Task Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="204"/>
+        <source>Dock style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="218"/>
+        <source>Glass 3D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="223"/>
+        <source>Glass 2D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="228"/>
+        <source>Flat 2D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="233"/>
+        <source>Metal 2D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="247"/>
+        <source>Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="263"/>
+        <source>Volume Control</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="279"/>
+        <source>Version Checker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="295"/>
+        <source>Wi-Fi Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="311"/>
+        <source>Battery Indicator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="327"/>
+        <source>Keyboard Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="343"/>
+        <source>Visibility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="357"/>
+        <source>Always Visible</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="362"/>
+        <source>Intelligent Auto Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="367"/>
+        <source>Auto Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/add_panel_dialog.ui" line="372"/>
+        <source>Always On Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AppearanceSettingsDialog</name>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="14"/>
+        <source>Appearance Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="45"/>
+        <source>Minimum icon size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="77"/>
+        <source>Maximum icon size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="109"/>
+        <source>Panel transparency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="122"/>
+        <source>Background color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="135"/>
+        <source>Border color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="148"/>
+        <source>Tooltip font size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="180"/>
+        <source>Icon spacing factor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="193"/>
+        <source>%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="240"/>
+        <source>Floating margin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="275"/>
+        <source>Indicator color (active)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="288"/>
+        <source>Indicator color (inactive)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="301"/>
+        <source>Bouncing launcher icon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="314"/>
+        <source>Show tooltip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="327"/>
+        <source>Zooming animation speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/appearance_settings_dialog.ui" line="365"/>
+        <source>Enable zooming effect</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationMenuSettingsDialog</name>
+    <message>
+        <location filename="../view/application_menu_settings_dialog.ui" line="14"/>
+        <source>Application Menu Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/application_menu_settings_dialog.ui" line="45"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/application_menu_settings_dialog.ui" line="68"/>
+        <source>Font size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/application_menu_settings_dialog.ui" line="81"/>
+        <source>Background transparency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/application_menu_settings_dialog.ui" line="113"/>
+        <source>%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/application_menu_settings_dialog.ui" line="132"/>
+        <source>Icon size</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditKeyboardLayoutsDialog</name>
+    <message>
+        <location filename="../view/edit_keyboard_layouts_dialog.ui" line="14"/>
+        <source>Edit Keyboard Layouts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_keyboard_layouts_dialog.ui" line="75"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_keyboard_layouts_dialog.ui" line="91"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_keyboard_layouts_dialog.ui" line="107"/>
+        <source>Languages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_keyboard_layouts_dialog.ui" line="120"/>
+        <source>Available Keyboard Layouts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_keyboard_layouts_dialog.ui" line="133"/>
+        <source>Keyboard Layouts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditLaunchersDialog</name>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="14"/>
+        <source>Edit Launchers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="45"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Tips: &lt;/p&gt;&lt;ul style=&quot;margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; -qt-list-indent: 1;&quot;&gt;&lt;li style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;To add a standard program launcher, either use drag-and-drop from any application list to the launcher list in this dialog, or right-click a running program on the dock panel then select &amp;quot;Pinned&amp;quot;.&lt;/li&gt;&lt;li style=&quot; margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Use drag-and-drop within the launcher list to re-order the items.&lt;/li&gt;&lt;/ul&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="64"/>
+        <source>Remove Selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="80"/>
+        <source>Remove All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="96"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A regular separator. Task icons for unpinned applications will be placed before this separator.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="99"/>
+        <source>Add Separator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="119"/>
+        <source>Add System Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="133"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A launcher separator. Task icons for unpinned applications will be placed after this separator.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/edit_launchers_dialog.ui" line="136"/>
+        <source>Add Launcher Separator</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TaskManagerSettingsDialog</name>
+    <message>
+        <location filename="../view/task_manager_settings_dialog.ui" line="14"/>
+        <source>Task Manager Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/task_manager_settings_dialog.ui" line="45"/>
+        <source>Showing tasks from:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/task_manager_settings_dialog.ui" line="58"/>
+        <source>Current desktop only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/task_manager_settings_dialog.ui" line="74"/>
+        <source>Current screen only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/task_manager_settings_dialog.ui" line="90"/>
+        <source>Group tasks by application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WallpaperSettingsDialog</name>
+    <message>
+        <location filename="../view/wallpaper_settings_dialog.ui" line="14"/>
+        <source>Wallpaper Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wallpaper_settings_dialog.ui" line="45"/>
+        <source>Browse Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wallpaper_settings_dialog.ui" line="68"/>
+        <source>Desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wallpaper_settings_dialog.ui" line="81"/>
+        <source>Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wallpaper_settings_dialog.ui" line="136"/>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WifiConnectionDialog</name>
+    <message>
+        <location filename="../view/wifi_connection_dialog.ui" line="14"/>
+        <source>Wi-Fi Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wifi_connection_dialog.ui" line="26"/>
+        <source>Wi-Fi password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wifi_connection_dialog.ui" line="52"/>
+        <source>Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wifi_connection_dialog.ui" line="78"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wifi_connection_dialog.ui" line="91"/>
+        <source>Signal strength</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wifi_connection_dialog.ui" line="142"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wifi_connection_dialog.ui" line="149"/>
+        <source>Disconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view/wifi_connection_dialog.ui" line="156"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
